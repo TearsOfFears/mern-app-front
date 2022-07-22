93c43bc2e322f0f4b5e279917cc371505be763f0
Nazar <nazardemchan@gmail.com>
2022-07-22 21:51:31 +0300

    clodinary feature

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -14,28 +14,53 @@ import { Typography } from "@mui/material";
 
 export const AddPost = () => {
 	const { isAuth } = useAuth();
-  const { id } = useParams();
-  console.log("isAuth",isAuth);
+	const { id } = useParams();
 	const [isLoading, setIsLoading] = useState(false);
 	const [imageURL, setImageUrl] = useState("");
 	const [text, setText] = useState("");
 	const [title, setTitle] = useState("");
 	const [tags, setTags] = useState("");
-	// const [data, setData] = useState(initialState);
+
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
 	const navigate = useNavigate();
-	const handleChangeFile = async (e) => {
-		try {
-			const formData = new FormData();
-			formData.append("image", e.target.files[0]);
-			const { data } = await axios.post("upload", formData);
-			setImageUrl(`http://localhost:4444${data.url}`);
-		} catch (err) {
-			console.log(err);
-			alert("Помилка при загрузці картинки");
-		}
-	};
+
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
+		if (!selectedFile) return;
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+            uploadImage(reader.result);
+        };
+        reader.onerror = () => {
+            console.error('AHHHHHHHH!!');
+            setErrMsg('something went wrong!');
+        };
+        setFileInputState(e.target.value);
+    };
+
+    const uploadImage = async (base64EncodedImage) => {
+        try {
+			const {data} = 	await axios.post("/api/upload", {data:base64EncodedImage});
+			setImageUrl(data);
+            setFileInputState('');
+            setPreviewSource('');
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+    };
+
+
 	const isEdit = Boolean(id);
 	const inputFileRef = useRef(null);
+
 	const onClickRemoveImage = async (event) => {
 		setImageUrl("");
 	};
@@ -79,7 +104,7 @@ export const AddPost = () => {
 			alert("Помилка при створенні статті");
 		}
 	};
-	
+
 	const onChange = React.useCallback((text) => {
 		setText(text);
 	}, []);
@@ -105,70 +130,70 @@ export const AddPost = () => {
 	if (isLoading) return <Typography>Loading...</Typography>;
 
 	return (
-    <Paper
-    style={{
-      padding: 30,
-    }}
-  >
-    <Button
-      variant="outlined"
-      size="large"
-      onClick={() => inputFileRef.current.click()}
-    >
-      Загрузить превью
-    </Button>
-    <input
-      type="file"
-      ref={inputFileRef}
-      onChange={handleChangeFile}
-      hidden
-    />{" "}
-    {imageURL && (
-      <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-        Удалить
-      </Button>
-    )}
-    {imageURL && (
-      <img className={styles.image} src={imageURL} alt="Uploaded" />
-    )}
-    <br />
-    <br />
-    <TextField
-      classes={{
-        root: styles.title,
-      }}
-      variant="standard"
-      placeholder="Заголовок статьи..."
-      fullWidth
-      name="title"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-    />
-    <TextField
-      classes={{
-        root: styles.tags,
-      }}
-      variant="standard"
-      placeholder="Тэги"
-      fullWidth
-      name="tags"
-      value={tags}
-      onChange={(e) => setTags(e.target.value)}
-    />
-    <SimpleMDE
-      className={styles.editor}
-      value={text}
-      onChange={onChange}
-      options={options}
-    />
-    <div className={styles.buttons}>
-      <Button size="large" variant="contained" onClick={onSubmit}>
-        {isEdit ? "Зберегти" : "Опубликовать"}
-      </Button>
-      <Link to="/">
-        <Button size="large">Отмена</Button>
-      </Link>
-    </div>
-  </Paper>
+		<Paper
+			style={{
+				padding: 30,
+			}}
+		>
+			<Button
+				variant="outlined"
+				size="large"
+				onClick={() => inputFileRef.current.click()}
+			>
+				Загрузить превью
+			</Button>
+			<input
+				type="file"
+				ref={inputFileRef}
+				onChange={handleFileInputChange}
+				hidden
+			/>{" "}
+			{imageURL && (
+				<Button variant="contained" color="error" onClick={onClickRemoveImage}>
+					Удалить
+				</Button>
+			)}
+			{imageURL && (
+				<img className={styles.image} src={imageURL} alt="Uploaded" />
+			)}
+			<br />
+			<br />
+			<TextField
+				classes={{
+					root: styles.title,
+				}}
+				variant="standard"
+				placeholder="Заголовок статьи..."
+				fullWidth
+				name="title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<TextField
+				classes={{
+					root: styles.tags,
+				}}
+				variant="standard"
+				placeholder="Тэги"
+				fullWidth
+				name="tags"
+				value={tags}
+				onChange={(e) => setTags(e.target.value)}
+			/>
+			<SimpleMDE
+				className={styles.editor}
+				value={text}
+				onChange={onChange}
+				options={options}
+			/>
+			<div className={styles.buttons}>
+				<Button size="large" variant="contained" onClick={onSubmit}>
+					{isEdit ? "Зберегти" : "Опубликовать"}
+				</Button>
+				<Link to="/">
+					<Button size="large">Отмена</Button>
+				</Link>
+			</div>
+		</Paper>
 	);
 };
